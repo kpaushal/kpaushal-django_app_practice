1aaa3ba38c177a2d9f893a99bf6eb3c2e66c2851
paushal <kpaushal9582@gmail.com>
2024-10-20 16:47:28 +0530

    last m changes

--- django_app/settings.py
+++ django_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG',False).lower()== True
 
-
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
